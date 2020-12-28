939dc6087b846d2194b9176d37572140552c2c87
Sam Gerrard <sgerrard@omnigenbiodata.co.uk>
2020-12-28 00:41:45 +0000

    moved files

---
+++ infrastructure/lib/CognitoStack.js
@@ -0,0 +1,54 @@
+import * as cognito from '@aws-cdk/aws-cognito';
+import * as sst from '@serverless-stack/resources';
+import {CfnOutput} from '@aws-cdk/core';
+import CognitoAuthRole from './CognitoAuthRole';
+
+export default class CognitoStack extends sst.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const app = this.node.root;
+
+    const userPool = new cognito.UserPool(this, 'UserPool', {
+      selfSignUpEnabled: true,
+      autoVerify: {email: true},
+      signInAliases: {email: true},
+    });
+
+    const userPoolClient = new cognito.UserPoolClient(this, 'UserPoolClient', {
+      userPool,
+      generateSecret: false,
+    });
+
+    const identityPool = new cognito.CfnIdentityPool(this, 'IdentityPool', {
+      allowUnauthenticatedIdentities: false,
+      cognitoIdentityProviders: [
+        {
+          clientId: userPoolClient.userPoolClientId,
+          providerName: userPool.userPoolProviderName,
+        },
+      ],
+    });
+
+    const authenticatedRole = new CognitoAuthRole(this, 'CognitoAuthRole', {
+      identityPool,
+    });
+
+    new CfnOutput(this, 'UserPoolId', {
+      value: userPool.userPoolId,
+    });
+
+    new CfnOutput(this, 'UserPoolClientId', {
+      value: userPoolClient.userPoolClientId,
+    });
+
+    new CfnOutput(this, 'IdentityPoolId', {
+      value: identityPool.ref,
+    });
+
+    new CfnOutput(this, 'AuthenticatedRoleName', {
+      value: authenticatedRole.role.roleName,
+      exportName: app.logicalPrefixedName('CognitoAuthRole'),
+    });
+  }
+}
---
+++ infrastructure/lib/index.js
@@ -0,0 +1,7 @@
+import DynamoDBStack from './DynamoDBStack';
+import CognitoStack from './CognitoStack';
+
+export default function main(app) {
+  new DynamoDBStack(app, 'dynamodb');
+  new CognitoStack(app, 'cognito');
+}
